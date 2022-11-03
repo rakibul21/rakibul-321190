673bfd6c2ce07042c592bf0590133733f930645b
Your Name <rakibulhasancse21@gmail.com>
2022-11-03 09:12:49 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $("#owl-demo").owlCarousel({
+
+        slideSpeed : 100,
+        paginationSpeed : 400,
+        items : 4,
+        autoPlay: true,
+        itemsDesktop : false,
+        itemsDesktopSmall : false,
+        itemsTablet: false,
+        itemsMobile : false
+
+    });
+
+});
